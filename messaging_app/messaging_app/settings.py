7e2534db6de78c681035dce86c8053f1421bdd57
aniebietafia <aniebietafia87@gmail.com>
2025-09-24 21:07:21 +0100

    added basic authentication to settings
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -62,7 +62,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
